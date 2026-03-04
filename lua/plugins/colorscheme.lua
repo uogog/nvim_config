aceb69a6ba78f6d44803d4946ec99f184874f1b0
uogog <w2013661951@qq.com>
2026-03-04 22:01:13 +0800

    gitignore上传，colorscheme更改

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,13 +3,8 @@
     { "ellisonleao/gruvbox.nvim" },
 
     -- Configure LazyVim to load gruvbox
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "gruvbox",
-        },
-    },
 }]]
+--
 --
 return {
     "taigrr/cyberpunk.nvim",
@@ -22,5 +17,11 @@ return {
         bold_functions = false, -- bold function names
         bold_keywords = true, -- bold keywords (default)
         overrides = {}, -- override specific highlight groups
+    },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "cyberpunk",
+        },
     },
 }
